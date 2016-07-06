be79ed9796170554504a422b63acfa11a50ae9d1
Nahuel <nahcar@gmail.com>
2016-07-06 11:19:49 -0400

    agregad settings.php

---
+++ settings.php
@@ -0,0 +1,5 @@
+<?php
+
+
+$settings['install_profile'] = 'standard';
+$config_directories['sync'] = 'sites/default/files/config_55Esvzfobw1FUi0szsFQv-ou23-krU29OKrigTaMJ8dKodbbETgn8mVAJM0Gawjm7pHUgNz1Rw/sync';
