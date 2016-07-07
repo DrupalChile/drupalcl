7a97efa7ff571047b210af9e6720cf893db520e4
Nahuel <nahcar@gmail.com>
2016-07-06 22:37:04 -0400

    agregué sal al settings

--- settings.php
+++ settings.php
@@ -1,5 +1,6 @@
 <?php
 
 
+$settings['hash_salt'] = 'OHRRfs_ZfmlJh0BTNnrjE4v3hdbPuOCpAZHEa2eE_KSNRYDhjY3qTJijmuwn4UMRuFlRx5ZQbg';
 $settings['install_profile'] = 'standard';
 $config_directories['sync'] = 'sites/default/files/config_55Esvzfobw1FUi0szsFQv-ou23-krU29OKrigTaMJ8dKodbbETgn8mVAJM0Gawjm7pHUgNz1Rw/sync';
